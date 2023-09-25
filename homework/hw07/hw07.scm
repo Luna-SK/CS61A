10bfe53122f30148192224104091e6e04f78c67a
luna <qi.jiandong.cn@gmail.com>
2023-09-25 10:04:36 +0800

    hw07 completed

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -1,11 +1,17 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst)
+  (cond ((or (null? asc-lst) (null? (cdr asc-lst))) #t)
+        ((> (car asc-lst) (cadr asc-lst)) #f)
+        (else (ascending? (cdr asc-lst)))))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+    (cond ((= exp 1) base)
+          ((odd? exp) (* base (pow base (- exp 1))))
+          ((even? exp) (square (pow base (/ exp 2))))))

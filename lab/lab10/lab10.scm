d4cd8adab47bfcfe66c092ded884bcfbd2c06269
luna <qi.jiandong.cn@gmail.com>
2023-09-25 16:11:15 +0800

    lab10 completed

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,9 +1,18 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        (else 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x))))
 
-(define lst 'YOUR-CODE-HERE)
+(define lst
+  (list (cons 1 nil) 2 (list 3 4) 5))
 
-(define (duplicate lst) 'YOUR-CODE-HERE)
+(define (duplicate lst)
+  (if (null? lst)
+    nil
+    (append (list (car lst) (car lst)) (duplicate (cdr lst)))))

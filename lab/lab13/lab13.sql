2df97ceb2d2ffa429660820c59d5c2da7efbcec7
luna <qi.jiandong.cn@gmail.com>
2023-10-02 13:23:01 +0800

    lab13 completed

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,36 +2,36 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color="blue" AND pet="dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song FROM students WHERE color="blue" AND pet="dog";
 
 
 CREATE TABLE smallest_int_having AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students GROUP BY smallest HAVING COUNT(*)=1;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet, a.song, a.color, b.color FROM students as a, students as b WHERE a.time<b.time and a.pet=b.pet and a.song=b.song;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven FROM students, numbers WHERE students.time=numbers.time and number=7 and "7"="True";
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, sum(MSRP)/count(*) as average_price FROM products GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, min(price) as lowest_price FROM inventory GROUP BY item;
 
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT item, store FROM products, lowest_prices WHERE item=name GROUP BY category HAVING min(MSRP/rating);
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT sum(Mbs) FROM shopping_list as a, stores as b WHERE a.store=b.store;
 
